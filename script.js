e39279cd28dffa439160f766a349003b98fe9837
thillo <salysem90@gmail.com>
2024-06-21 23:52:19 +0200

    correction

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
-let menu = document.getElementById("menu")
-menu.style.maxHeight="0px"
+// let menuList = document.getElementById("menuList")
+// menuList.style.maxHeight="0px"
 
-function toggleMenu(){
-    if (menu.style.maxHeight == "0px") {
-        menu.style.maxHeight="300px";
-    }
-    else{
-        menu.style.maxHeight="0px";
-    }
+// function toggleMenu(){
+//     if (menu.style.maxHeight == "0px") {
+//         menu.style.maxHeight="300px";
+//     }
+//     else{
+//         menu.style.maxHeight="0px";
+//     }
+// }
+
+hamburger = document.querySelector(".bx-menu");
+hamburger.onclick = function () {
+    menu = document.querySelector(".menu");
+    menu.classList.toggle("active");
 }
